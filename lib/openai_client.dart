1b339761a8e4bbc7dd1d867a6797ee9bf5059f9d
Francesco Coppola <f.coppola1998@gmail.com>
2022-11-16 00:31:55 +0100

    feat: export completion

--- lib/openai_client.dart
+++ lib/openai_client.dart
@@ -1,6 +1,8 @@
 library openai_client;
 
 export 'src/client.dart';
+export 'src/completion.dart';
+export 'src/completion.dart';
 export 'src/configuration.dart';
 export 'src/logger/logger.dart';
 export 'src/model/model.dart';
