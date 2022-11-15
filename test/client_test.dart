02e5fdd6c4fdc00c16a5825bc819c51989aef113
Francesco Coppola <f.coppola1998@gmail.com>
2022-11-15 19:55:24 +0100

    test: close http connection

--- test/client_test.dart
+++ test/client_test.dart
@@ -1,8 +1,21 @@
+import 'package:openai_client/openai_client.dart';
 import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  test('Client test', () {
-    expect(true, isNotNull);
+  test('Executing requests after client has been closed should throw',
+      () async {
+    final client = OpenAIClient(
+      configuration: const OpenAIConfiguration(
+        apiKey: '',
+        organizationId: '',
+      ),
+    );
+
+    client.close();
+
+    final req = client.models.list();
+
+    expect(req, throwsStateError);
   });
 }
